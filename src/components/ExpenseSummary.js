d514af7f197f59f62911ec5b63112ee0e92c24f0
Stephane Moser <moser.sts@gmail.com>
2019-02-25 17:58:06 +0000

    Styling components

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total'
@@ -7,14 +8,16 @@ import 'numeral/locales.js'
 numeral.locale('pt-pt')
 
 export const ExpenseSummary = ({ expensesCount, expensesTotal}) => {
-  let totalMessage
-  if(expensesCount > 0){
-    const total = numeral(expensesTotal / 100 ).format('$0,0.00')
-    totalMessage = `totalling ${total}`
-  }
+  const total = expensesCount > 0 ? numeral(expensesTotal / 100 ).format('$0,0.00') : undefined
   const expenseWord = expensesCount === 1 ? 'expense': 'expenses'
-  return (  <div>
-    <h1>Viewing {expensesCount} {expenseWord} {totalMessage && totalMessage}</h1>
+  return (  
+  <div className="page-header">
+    <div className="content-container">
+      <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} {total && 'totalling'} {total && <span>{total}</span>}</h1>
+      <div className="page-header__actions">
+      <Link className="button" to="/add" >Add Expense</Link>
+      </div>
+    </div>
   </div>)
 }
 
